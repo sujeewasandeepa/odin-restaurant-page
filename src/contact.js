31ed0c22737963f3993667b8e2c7dd060daa2ee5
Jayantha Kularathna <sujeewasandeepa@protonmail.ch>
2022-03-31 20:34:59 +0530

    minor changes

--- src/contact.js
+++ src/contact.js
@@ -4,7 +4,13 @@ import destroy from "./destroyTab";
 export default function renderContact (id) {
     destroy();
     const subHeading = document.createElement('h2');
+    const workInProgress = document.createElement('marquee');
+
+    workInProgress.scrollAmount = 20;
+    workInProgress.innerText = "<< work in progress! >>";
+
     subHeading.innerText = id;
     subHeading.className = 'subHeading';
-    tabBody.append(subHeading);
+
+    tabBody.append(subHeading, workInProgress);
 }
