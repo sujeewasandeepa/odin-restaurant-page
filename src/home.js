d1d22d862f315afbcff29599eed10ed5f3df8fa1
Jayantha Kularathna <sujeewasandeepa@protonmail.ch>
2022-03-31 20:04:18 +0530

    minor improvements

--- src/home.js
+++ src/home.js
@@ -1,17 +1,25 @@
 import { tabBody } from ".";
 import destroy from "./destroyTab";
+import coffee from "./coffee.png";
 
 export default function renderHome (id) {
     destroy();
     const subHeading = document.createElement('h2');
-    subHeading.innerText = id;
+    const hr = document.createElement('hr');
+    subHeading.innerText = "CHILL-ZONE";
     subHeading.className = 'subHeading';
-    tabBody.append(subHeading);
+    tabBody.append(subHeading, hr);
 
     const tabContent = document.createElement('div');
     const greeting = document.createElement('h3');
     const description = document.createElement('p');
     const schedule = document.createElement('p');
+    const coffeeImg = new Image();
+
+    tabContent.className = "tabContent";
+    
+    coffeeImg.src = coffee;
+    coffeeImg.className = "coffeeImg";
 
     greeting.id = 'greeting';
     greeting.innerHTML = "Hello Welcome to <br> the Colombo Chill Zone!";
@@ -26,6 +34,6 @@ export default function renderHome (id) {
     + "Everyday!"
     schedule.className = "schedule";
 
-    tabContent.append(greeting, description, schedule);
+    tabContent.append(greeting, description, schedule, coffeeImg);
     tabBody.append(tabContent);
 }
