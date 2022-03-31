8747909fead2c0749f643bea011ebae3fac0ecae
Jayantha Kularathna <sujeewasandeepa@protonmail.ch>
2022-03-31 19:55:33 +0530

    complete home tab

--- src/home.js
+++ src/home.js
@@ -7,14 +7,25 @@ export default function renderHome (id) {
     subHeading.innerText = id;
     subHeading.className = 'subHeading';
     tabBody.append(subHeading);
+
     const tabContent = document.createElement('div');
     const greeting = document.createElement('h3');
     const description = document.createElement('p');
+    const schedule = document.createElement('p');
+
     greeting.id = 'greeting';
     greeting.innerHTML = "Hello Welcome to <br> the Colombo Chill Zone!";
-    description.innerHTML = "If you are looking for a chill place to hangout with your loved ones."
-    + "";
-    tabContent.append(greeting);
-    tabContent.append(description);
+
+    description.innerHTML = "If you are looking for a chill place to hangout "
+    + "with your loved ones. Look no further! "
+    + "We have comfy and relaxing lounges for everyone! ";
+    description.className = "tabDescription";
+
+    schedule.innerHTML = "We are open from <br>"
+    + "8:00 AM - 10:00 PM <br>"
+    + "Everyday!"
+    schedule.className = "schedule";
+
+    tabContent.append(greeting, description, schedule);
     tabBody.append(tabContent);
 }
